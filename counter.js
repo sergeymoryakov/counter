a5ee6c37ca9ef7e381e18cd3705ec7f611a51f64
Sergey Moryakov <sergey@moryakov.com>
2023-05-17 18:33:38 +0300

    Comment Console Commands

--- counter.js
+++ counter.js
@@ -6,9 +6,9 @@ const deductTwo = document.querySelector("#deductTwoButton");
 const reset = document.querySelector("#resetButton");
 
 // data-attributes example
-console.log(displayCount.dataset);
-console.log(addOne.dataset);
-console.log(addTwo.dataset);
+// console.log(displayCount.dataset);
+// console.log(addOne.dataset);
+// console.log(addTwo.dataset);
 
 const COUNTER_INIT_VALUE = 0;
 let counter = COUNTER_INIT_VALUE;
@@ -17,31 +17,31 @@ displayCount.innerText = counter;
 addOne.addEventListener('click', function() {
     counter = counter + parseInt(addOne.dataset.button);
     displayCount.innerText = counter;
-    console.log(counter);
+    // console.log(counter);
 });
 
 addTwo.addEventListener('click', function() {
     counter = counter + parseInt(addTwo.dataset.button);
     displayCount.innerText = counter;
-    console.log(counter);
+    // console.log(counter);
 });
 
 deductOne.addEventListener('click', function() {
     counter = counter - parseInt(addOne.dataset.button);
     displayCount.innerText = counter;
-    console.log(counter);
+    // console.log(counter);
 });
 
 deductTwo.addEventListener('click', function() {
     counter = counter - parseInt(addTwo.dataset.button);
     displayCount.innerText = counter;
-    console.log(counter);
+    // console.log(counter);
 });
 
 reset.addEventListener('click', function() {
     counter = COUNTER_INIT_VALUE;
     displayCount.innerText = counter;
-    console.log(counter);
+    // console.log(counter);
 });
 
 // console.log(document);
